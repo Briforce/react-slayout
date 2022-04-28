8a5162d5b93bcbf990cb217a47606f474316b522
Brian Parker <BParker222@gmail.com>
2022-04-27 23:14:35 -0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Frame } from './react-slayout';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className='FrameContainer'>
+          <Frame />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/react-slayout/Frame.tsx
@@ -0,0 +1,5 @@
+import './Frame.css';
+
+const Frame = () => <div className="Frame">Frame</div>
+
+export default Frame
